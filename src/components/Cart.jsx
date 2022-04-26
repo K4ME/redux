9e8c11652d89faa7b050b9e411851f391a398db0
Guilherme Macrini <guilhermegodencio@gmail.com>
2022-04-26 11:06:01 -0300

    adding immer to project

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const Cart = () => {
-  const state = useSelector((state) => state);
-
-  console.log(state);
+  const cart = useSelector((state) => state.cart.items);
 
   return (
     <table>
@@ -16,7 +14,16 @@ const Cart = () => {
           <th>Subtotal</th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        {cart.map((item) => (
+          <tr key={item.product.id}>
+            <td>{item.product.title}</td>
+            <td>{item.product.price}</td>
+            <td>{item.quantity}</td>
+            <td>{(item.product.price * item.quantity).toFixed(2)}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,31 +1,28 @@
 import { Reducer } from "redux";
+import produce from "immer";
 
 const INITIAL_STATE = {
   items: [],
 };
 
 const cart = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case "ADD_PRODUCT_TO_CART": {
-      const { product } = action.payload;
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case "ADD_PRODUCT_TO_CART": {
+        const { product } = action.payload;
 
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
-            product,
-            quantity: 1,
-          },
-        ],
-      };
-    }
-    default: {
-      return state;
-    }
-  }
+        draft.items.push({
+          product,
+          quantity: 1,
+        });
 
-  return state;
+        break;
+      }
+      default: {
+        return draft;
+      }
+    }
+  });
 };
 
 export default cart;
